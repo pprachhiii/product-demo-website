a90b1d62d2837358892d6457bb3611e6f323fc48
Prachi Yadav <prachiiyadav2409@gmail.com>
2025-06-05 17:45:22 +0530

    setting up

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.VITE_BASE_URL + "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
